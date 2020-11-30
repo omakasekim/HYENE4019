2ae4257ca6d9ee08c8a7ae28230e81abadf0e554
김스시 <57784879+omakasekim@users.noreply.github.com>
2020-12-01 03:07:50 +0900

    Update ResponsePacket.java

--- FTP/ResponsePacket.java
+++ FTP/ResponsePacket.java
@@ -4,7 +4,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
-public class ResponsePacket {
+public class ResponsePacket implements Comparable<ResponsePacket> {
 
     public static final int maxChunkSize = 3;
 
@@ -17,6 +17,12 @@ public class ResponsePacket {
         this.chkSum = 0x0000;
     }
 
+    public ResponsePacket(byte seqNo, byte chkSum) {
+        this.seqNo = seqNo;
+        this.chkSum = 0x0000;
+    }
+
+
     public ResponsePacket(byte[] bytes) {
         ByteBuffer wrapped = ByteBuffer.wrap(bytes);
         this.seqNo = wrapped.get();
@@ -28,8 +34,16 @@ public class ResponsePacket {
         dataOutputStream.writeShort(chkSum);
     }
 
+    public byte getSeqNo(){ return seqNo; }
     public boolean calcCHK() {
         return chkSum != 0x000;
+    }
+
+    @Override
+    public int compareTo(ResponsePacket comparePacket) {
+        if(this.getSeqNo() - comparePacket.getSeqNo() < 0) return -1;
+        else if(this.getSeqNo() - comparePacket.getSeqNo() > 0) return 1;
+        else return 0;
     }
 
 }
